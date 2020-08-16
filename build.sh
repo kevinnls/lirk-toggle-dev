eb497fc108f1ee637f9e4114963422180981a0fa
kevinnls <57634663+kevinnls@users.noreply.github.com>
2020-08-16 23:09:20 +0530

    wip build.sh

--- build.sh
+++ build.sh
@@ -1,36 +1,78 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-srcdir="./src"
-builddir="./mbuild"
-modulefiles=("LirkToggle.js","LirkToggle.css")
+# badges
+good="   \e[1;32m+\e[0m"
+bad="   \e[1;31m-\e[0m"
+# checks
+srcd="./src"
+buildd="./mbuild"
+gitd="${build}/.git"
+modulefiles=(
+    "package.json"
+    "README.md"
+    "${srcd}/LirkToggle.js"
+    "${srcd}/LirkToggle.css"
+)
 
-echo "this is to remind you to run tests"
-echo "if you haven't run them, you should do that now"
-read -p "proceed with build? [y/n] " y_n
+
+yn_eval(){
 case $y_n in
     y)
-        break
         ;;
     n)
         exit 1
         ;;
 esac
+}
 
 clear
-echo "starting build..."
-if [ ! -d "$srcdir" ]; then
-    echo
-    echo "there's no src dir. not sure what to do"
-    echo "so exiting"
+echo -e "\e[1;33mstarting custom build script\e[0m"
+echo -e "\e[32mrunning checks...\e[0m"
+
+fails=0
+
+if [ ! -f "./package.json" ]; then
+    echo -e "$bad \e[31mpackage.json\e[0m"
+    fails=$((++fails))
+else 
+    echo -e "$good \e[32mpackage.json\e[0m"
+fi
+
+if [ ! -d "$srcd" ]; then
+    echo -e "$bad \e[31msrc dir\e[0m"
+    fails=$((++fails))
+else 
+    echo -e "$good \e[32msrc dir\e[0m"
+fi
+
+if [ ! -d "$buildd" ]; then
+    mkdir "$buildd" -p
+    echo -e "$good \e[32mbuild dir\e[0m"
+else
+    echo -e "$good \e[32mbuild dir\e[0m"
+fi
+
+if [ ! -d "${buildd}/.git" ]; then
+    git init $buildd > /dev/null
+    echo -e "    $good \e[32mis a git repo\e[0m"
+else
+    echo -e "    $good \e[32mis a git repo\e[0m"
+fi
+
+(cd "${buildd}";
+if [ -n $(git remote) ]; then
+    git remote get-url "$(git remote)"
+fi
+) | read giturl
+echo $giturl
+if [ $fails -ne 0 ]; then
+    echo -e "\e[31msome checks have failed. build cannot complete\e[0m"
     exit 1
 fi
-if [ ! -d "$builddir" ]; then
-    mkdir "$buildddir" -p
-fi
-echo checking files...
-if [ ! -f "./package.json" ]; then
-    echo "npm needs a package.json file"
-    read -p "would you like to create one? [y/n] " y_n
-        
-for f in "$modulefiles"; 
-do if [ ! -f "$srcdir/$f"]; 
+    
+#echo -e "\e[1;34mthis is to remind you to run tests\e[0m"
+#echo -e "if you haven't run them, you should do that now"
+#read -p "proceed with build? [y/n] " y_n
+
+#for f in "$modulefiles"; 
+#do if [ ! -f "$srcdir/$f"]; 
